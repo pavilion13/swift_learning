385c804920a65943d5ba218fd7fdda182866eb9c
Pavel Dmitriev <Dmitriev.P.A@sberbank.ru>
2021-10-14 15:29:34 +0300

    complete Lesson 4 challenge

--- simpleApp/simpleApp/ContentView.swift
+++ simpleApp/simpleApp/ContentView.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").foregroundColor(Color.white)
-            .padding()
-            .background(Color.green).cornerRadius(10.0).padding().background(Color.blue).cornerRadius(10)
+        VStack{
+            Spacer()
+            
+            ZStack{
+                Image("10437190_original").resizable().aspectRatio(contentMode: .fit).cornerRadius(10).padding()
+                VStack{
+                    Text("New York").padding(2)
+                    Text("USA").font(.system(size: 10))
+                }.padding().background(Color.black).foregroundColor(.white).cornerRadius(10).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+            }
+            
+            Spacer()
+            ZStack{
+                Image("1466855827_00").resizable().aspectRatio(contentMode: .fit).cornerRadius(10).padding()
+                VStack{
+                    Text("London").padding(2)
+                    Text("UK").font(.system(size: 10))
+                }.padding().background(Color.black).foregroundColor(.white).cornerRadius(10).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+            }
+            
+            Spacer()
+        }
     }
 }
 
